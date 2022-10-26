ede434d9b2553bb6fd3e6966de298fa29932f0e8
hainkiwanki <kevinr.be@gmail.com>
2022-10-26 20:06:59 +0200

    to pc

--- src/main/java/com/hainkiwanki/geneticsmod/item/custom/DnaSamplerItem.java
+++ src/main/java/com/hainkiwanki/geneticsmod/item/custom/DnaSamplerItem.java
@@ -1,5 +1,6 @@
 package com.hainkiwanki.geneticsmod.item.custom;
 
+import com.hainkiwanki.geneticsmod.util.Utils;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -7,6 +8,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShearsItem;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class DnaSamplerItem extends Item {
@@ -20,14 +22,20 @@ public class DnaSamplerItem extends Item {
     @Override
     public InteractionResult interactLivingEntity(ItemStack pStack, Player pPlayer, LivingEntity pInteractionTarget, InteractionHand pUsedHand) {
         if(!pPlayer.level.isClientSide() && pUsedHand == InteractionHand.MAIN_HAND) {
-            String item = ForgeRegistries.ITEMS.getKey(pStack.getItem()).getPath();
-            pPlayer.sendMessage(new TextComponent(item), pPlayer.getUUID());
-            /*if(entityTpes == null) {
-                return InteractionResult.FAIL;
-            } else {
-                boolean checkEntity = pInteractionTarget.getType().is(entityTpes);
-                pPlayer.sendMessage(new TextComponent("Mob is " + ((checkEntity) ? "correct" : "incorrect" + " entity")), pPlayer.getUUID());
-            }*/
+            var entityTypes = Utils.entityTypesPerItem.get(pStack.getItem());
+            boolean checkEntity = pInteractionTarget.getType().is(entityTypes);
+            pPlayer.sendMessage(new TextComponent("Mob is " + ((checkEntity) ? "correct" : "incorrect" + " entity")),
+                    pPlayer.getUUID());
+
+            // TODO: Drop item
+            /*java.util.List<ItemStack> drops = target.onSheared(playerIn, stack, entity.level, pos,
+                    net.minecraft.world.item.enchantment.EnchantmentHelper.getItemEnchantmentLevel(net.minecraft.world.item.enchantment.Enchantments.BLOCK_FORTUNE, stack));
+            java.util.Random rand = new java.util.Random();
+            drops.forEach(d -> {
+                net.minecraft.world.entity.item.ItemEntity ent = entity.spawnAtLocation(d, 1.0F);
+                ent.setDeltaMovement(ent.getDeltaMovement().add((double)((rand.nextFloat() - rand.nextFloat()) * 0.1F), (double)(rand.nextFloat() * 0.05F), (double)((rand.nextFloat() - rand.nextFloat()) * 0.1F)));
+            });
+            stack.hurtAndBreak(1, playerIn, e -> e.broadcastBreakEvent(hand));*/
         }
         return InteractionResult.CONSUME;
     }
--- src/main/java/com/hainkiwanki/geneticsmod/util/Utils.java
+++ src/main/java/com/hainkiwanki/geneticsmod/util/Utils.java
@@ -1,18 +1,20 @@
 package com.hainkiwanki.geneticsmod.util;
 
+import com.hainkiwanki.geneticsmod.item.ModItems;
+import com.hainkiwanki.geneticsmod.tags.ModTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.HashMap;
 
 public class Utils {
-    public static HashMap<String, TagKey<EntityType<?>>> entityTypesPerItem = new HashMap<>();
-
-    public static void registerSampler(ItemStack pItemStack) {
-        String item = ForgeRegistries.ITEMS.getKey(pItemStack.getItem()).getPath();
-        if(!entityTypesPerItem.containsKey(item)) {
-        }
+    public static HashMap<Item, TagKey<EntityType<?>>> entityTypesPerItem = new HashMap<>();
+    static {
+        entityTypesPerItem.put(ModItems.DNA_SAMPLER_CLIPBONE.get(), ModTags.EntityTypeTags.CAN_CLIPBONE);
+        entityTypesPerItem.put(ModItems.DNA_SAMPLER_KNIFE.get(), ModTags.EntityTypeTags.CAN_KNIFE);
+        entityTypesPerItem.put(ModItems.DNA_SAMPLER_SWAB.get(), ModTags.EntityTypeTags.CAN_SWAB);
+        entityTypesPerItem.put(ModItems.DNA_SAMPLER_SYRINGE.get(), ModTags.EntityTypeTags.CAN_SYRINGE);
     }
 }
